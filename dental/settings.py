bbfab66221c9c3599d0a66e9900db7150b8c97c4
sudharsan004 <sudharsansriram8921@gmail.com>
2020-05-28 12:33:46 +0530

    set up for heroku

--- dental/settings.py
+++ dental/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dental.urls'
@@ -123,6 +127,8 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressMainfestStaticFilesStorage'
+
 EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
@@ -130,4 +136,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "8921test@gmail.com"
 EMAIL_HOST_PASSWORD = 'rodwwgoazpdjcspw'
 #pass8921 calendar-jopchlagfiwotbgo
+
+django_heroku.settings(locals())
 
